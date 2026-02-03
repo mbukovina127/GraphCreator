1928b451fe25e008f5a12d32090654505ff20639
CLGIN <xbukovinam@stuba.sk>
2026-02-03 22:35:32 +0100

    [WIP]Feature: function and variable processing almost complete

--- src/ray_implementation/cpg_builder.py
+++ src/ray_implementation/cpg_builder.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List
 
 from .ast_utils import ASTUtils
 from .local_output_builder import LocalOuputBuilder
-from .local_symbol_table import SymbolTable, Scope
+from .local_symbol_table import SymbolTable
 
 
 class CPGBuilder:
@@ -13,7 +13,9 @@ class CPGBuilder:
         self.local_builder = local_builder
         self.lst = lst
         self.scope_stack: List[str] = []
-        self.astId_nodeId_map: Dict[str, str] = {}
+        self.context_stack: List[str] = ["global"] # TODO replace with enum and repleca initialization
+        self.context_relevant_node_id: List[str] = [""]
+        self.astId_nodeId_map: Dict[str, str] = {} # TODO move this to local builder
 
         self.knowledge_nodes = self.local_builder.get_collection("knowledge_nodes")
         self.knowledge_edges = self.local_builder.get_collection("knowledge_edges")
@@ -35,6 +37,7 @@ class CPGBuilder:
     def _push_scope(self, s_id: str):
         self.scope_stack.append(s_id)
         return
+    
     def _pop_scope(self):
         return self.scope_stack.pop()
     
@@ -44,7 +47,7 @@ class CPGBuilder:
             "_key": node_id,
             "symbol_id": node.id,
             "type": node.type,
-            "text": node.text.decode("utf-8") if isinstance(node.text, bytes) else node.text,
+            "text": ASTUtils.get_text(node),
             "start_byte": node.start_byte,
             "end_byte": node.end_byte,
             "file_path": file_path,
@@ -68,40 +71,96 @@ class CPGBuilder:
         }
         self.knowledge_edges.insert(edge)
         return edge
-    
+
+    def create_relation_if_possible(self, node, file_path: str):
+        """
+            Creates relation between edges
+            Handles context dependent nodes
+        """
+        k_node = None
+        recursive: bool = False
+
+
+        type = ASTUtils.is_relation_node(node)
+        if type is not None:
+            # cookie cutter variable identifier
+            if type == 'ident': # TODO no paramater and argument types
+                k_node = self._create_knowledge_node(node, file_path)
+
+                symbol = self.lst.scope_lookup_by_name(self.scope_stack[-1], ASTUtils.get_text(node))
+                if symbol is not None:
+                    found_node_id = self.astId_nodeId_map[str(symbol.ast_id)]
+                    self._create_knowledge_edge(k_node["_key"], found_node_id, "refers_to") # TODO later more edge types with helper functions less hardocded?
+
+            #recursive function call
+            if type == 'call':
+                k_node = self._create_knowledge_node(node, file_path)
+
+                name = ASTUtils.get_text(ASTUtils.first_node_of_type(node, "identifier"))
+                definition = self.lst.scope_lookup_by_name(self.scope_stack[-1], name)
+                if definition is not None:
+                    found_node_id = self.astId_nodeId_map[str(definition.ast_id)]
+                    self._create_knowledge_edge(found_node_id, k_node["_key"], "defines")
+                else:
+                    # TODO unresolved edge
+                    pass
+
+                arguments = ASTUtils.first_node_of_type(node, "arguments")
+                if arguments.child_count > 2: # parenthesis count as children
+                    self.context_stack.append('arguments')
+                    self.context_relevant_node_id.append(k_node["_key"]) # link to the function call
+                    for arg in arguments.children:
+                        self.build_cpg(arg, file_path)
+                    recursive = True
+                    self.context_stack.pop()
+                    self.context_relevant_node_id.pop()
+
+            #===========================================
+            # Context dependant edges
+            #===========================================
+            if k_node is not None: # meaning a node was created at some point so it needs to be linked to the relevant node
+                if self.context_stack[-1] == 'arguments':
+                    relevant_id = self.context_relevant_node_id[-1]
+                    self._create_knowledge_edge(relevant_id, k_node["_key"], "has_argument") # directional edge
+
+
+            if recursive: # doesn't feel good
+                return True
+        return False
+
+    def create_knowledge_node_if_possible(self, node, file_path: str) -> bool:
+        """
+            ONLY creates nodes that are in symbol table ~mostly~
+        """
+        if ASTUtils.is_knowledge_node(node):
+            k_node = self._create_knowledge_node(node, file_path)
+            # adding contains/implements edges for files
+            if k_node["type"] in ["function_declaration", "variable_declaration"]:
+                root_chunk_id = self.astId_nodeId_map.get(str(self.scope_stack[0]))  # FIXME hardocoded
+                if root_chunk_id is not None:
+                    self._create_knowledge_edge(root_chunk_id, k_node["_key"],"contains")  # TODO temporary "contains" relation type
+
+            if k_node["type"] == "block":
+                symbol = self.lst.scope_lookup_by_name(self.scope_stack[-1], self.scope_stack[-1])  # TODO test if this finds the correct function or control structure
+
+
     def build_cpg(self, node, file_path: str):
         """
         Build the CPG from the AST node and local symbol table
         """
-        k_node = None 
+
 
         # pushes scope stack if needed
         if ASTUtils.is_different_scope_node(node):
             self._push_scope(node.id)
 
+        if self.create_knowledge_node_if_possible(node, file_path):
+            return
 
-        if ASTUtils.is_knowledge_node(node):
-            k_node = self._create_knowledge_node(node, file_path)
-            if k_node["type"] in ["function_declaration", "variable_declaration"]:
-                root_chunk = self.lst.scope_lookup(self.scope_stack[-1], self.scope_stack[0]) # FIXME very assignment to file/chunk
-                if root_chunk is not None:
-                    root_node_id = self.astId_nodeId_map.get(str(root_chunk.ast_id))
-                    if root_node_id is not None:
-                        self._create_knowledge_edge(root_node_id, k_node["_key"], "contains") #TODO temporary "contains" relation type
-
-            if k_node["type"] == "block":
-                self.lst.scope_lookup(self.scope_stack[-1], self.scope_stack[-1]) #TODO test if this finds the correct function or control structure
-                pass # TODO: somehow map to to function or control structure decleration
         
-        # adding to local symbol table
-        if ASTUtils.is_reference_node(node):
-            k_node = self._create_knowledge_node(node, file_path)
-
-            symbol = self.lst.scope_lookup(self.scope_stack[-1], node.text.decode("utf-8") if isinstance(node.text, bytes) else node.text)
-            
-            if symbol is not None and k_node is not None:
-                node_id = self.astId_nodeId_map[str(symbol.ast_id)]
-                self._create_knowledge_edge(node_id, k_node["_key"], "declares") # TODO declares for now, later more edge types with helper functions
+        # adding reference edges and nodes
+        if self.create_relation_if_possible(node, file_path):
+            return
 
         # walk
         for child in node.children:
--- tests/test_symbol_table.py
+++ tests/test_symbol_table.py
@@ -8,13 +8,50 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 from code_analyzer import ASTManager
 from ray_implementation import SymbolBuilder, CPGBuilder, LocalOuputBuilder, SymbolTable
 
-SAMPLE_LUA_VERY_SIMPLE = """
+SAMPLE_LUA_VAR_VERY_SIMPLE = """
 local a = 5
 a = 1
 """
 
+# 3 symbols, 2 scope, 2 contains, 4 referes_to edges, 2 has_argument edges.... a lot is going on
+SAMPLE_LUA_FUN_VERY_SIMPLE = """
+local function add(a,b)
+    return a + b
+add(a,b)
+"""
+
+SAMPLE_LUA_VAR_SIMPLE = """
+local x = 10
+local y = 1 + 1
+local z = y + 5
+local w
+
+GLOBAL = 100
+
+x = 1
+y = 2
+w = 10
+
+-- undefined
+a = x
+"""
+
+SAMPLE_LUA_BASIC = """
+-- 
+local x = 10
+local y = 5
+
+-- function that adds two numbers
+local function add(a,b)
+    local result
+    result = a + b
+    return result
+
+print(add(x,y))
+"""
+
 with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
-    f.write(SAMPLE_LUA_VERY_SIMPLE)
+    f.write(SAMPLE_LUA_BASIC)
     f.flush()
     
     ast = ASTManager().parse(f.name)
@@ -29,6 +66,82 @@ with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
 
     knowledge_graph_creator = CPGBuilder(localBuilder, lst)
     knowledge_graph_creator.build_cpg(ast.root_node, f.name)
-    print()
+
+    print(lst.exports.__len__())
 
 
+class TestSymbolCreation:
+
+    def test_simple_variable_declaration(self):
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
+            f.write(SAMPLE_LUA_VAR_VERY_SIMPLE)
+            f.flush()
+            parser = ASTManager()
+            builder = LocalOuputBuilder()
+            lst = SymbolTable("1")
+            ast = parser.parse(f.name)
+
+            symbolmanager = SymbolBuilder(local_builder=builder, lst=lst, file_path=f.name)
+
+            symbolmanager.walk(ast.root_node)
+
+            assert lst.exports.__len__() == 1  # 'a' declared and assigned
+            assert lst.scopes.__len__() == 1
+
+
+    def test_simple_function_definition(self):
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
+            f.write(SAMPLE_LUA_FUN_VERY_SIMPLE)
+            f.flush()
+            parser = ASTManager()
+            builder = LocalOuputBuilder()
+            lst = SymbolTable("1")
+            ast = parser.parse(f.name)
+
+            symbolmanager = SymbolBuilder(local_builder=builder, lst=lst, file_path=f.name)
+
+            symbolmanager.walk(ast.root_node)
+
+            assert lst.exports.__len__() == 3  # 3 symbols
+            assert lst.scopes.__len__() == 2
+
+class TestCPGBuilder:
+
+    def test_simple_variable(self):
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
+            f.write(SAMPLE_LUA_VAR_VERY_SIMPLE)
+            f.flush()
+            parser = ASTManager()
+            builder = LocalOuputBuilder()
+            lst = SymbolTable("1")
+            ast = parser.parse(f.name)
+            cpg_builder = CPGBuilder(builder, lst)
+
+            symbolmanager = SymbolBuilder(local_builder=builder, lst=lst, file_path=f.name)
+
+            symbolmanager.walk(ast.root_node)
+
+            cpg_builder.build_cpg(ast.root_node, f.name)
+
+            assert builder._knowledge_nodes.__len__() == 4 # chunk and variable declaration + 2 identifiers
+            assert builder._knowledge_edges.__len__() == 3 # chunk contains variable, variable declared, variable assigned
+
+    #TODO add block processing return statement, parameter relation, unresolved edges
+    def test_simple_function(self):
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.lua', delete=False) as f:
+            f.write(SAMPLE_LUA_FUN_VERY_SIMPLE)
+            f.flush()
+            parser = ASTManager()
+            builder = LocalOuputBuilder()
+            lst = SymbolTable("1")
+            ast = parser.parse(f.name)
+            cpg_builder = CPGBuilder(builder, lst)
+
+            symbolmanager = SymbolBuilder(local_builder=builder, lst=lst, file_path=f.name)
+
+            symbolmanager.walk(ast.root_node)
+
+            cpg_builder.build_cpg(ast.root_node, f.name)
+
+            print(builder._knowledge_nodes.__len__())
+            print(builder._knowledge_edges.__len__())
